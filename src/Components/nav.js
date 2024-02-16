7f946758ec34e50624d961358f1987538118981c
JohannHerberger <j.herberger50@gmail.com>
2024-02-16 17:56:32 +0100

    Respponsive Navbar

--- src/Components/nav.js
+++ src/Components/nav.js
@@ -1,19 +1,31 @@
 import "../styles/nav.css"
 import logo from "../images/Logo.svg"
+import hamburger from "../images/icons8-hamburger-50.png"
 
 export function Nav (){
     return(
         <nav className="navbar">
-            <div></div>
             <img className="img" src={logo} alt="logo"></img>
-            <div className="navigation-elements">
-                <a>Home</a>
-                <a>About</a>
-                <a>Reservation</a>
-                <a>Oder Online</a>
-                <a>Login</a>
-            </div>
-            <div></div>
+            <button className="toggle-button">
+                <img className="hamburger" src={hamburger} />
+            </button>
+            <ul className="navigation-elements">
+                <li>
+                    <a>Home</a>
+                </li>
+                <li>
+                    <a>About</a>
+                </li>
+                <li>
+                    <a>Reservation</a>
+                </li>
+                <li>
+                    <a>Order online</a>
+                </li>
+                <li>
+                    <a>Login</a>
+                </li>
+            </ul>
         </nav>
     )
 }
