a4bfc0e6666002b7dabf45925b56d3a1da856034
JohannHerberger <j.herberger50@gmail.com>
2024-02-16 23:52:20 +0100

    Header ohne responsive design

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,9 +1,21 @@
 import "../styles/header.css";
+import headerLogo from "../images/Bildschirmfoto 2024-02-16 um 23.33.08.png"
 
 export function Header (){
     return(
         <header className="header">
-            Head
+            <div className="header-text-container">
+                <h1 className="header-h1">Little Lemon</h1>
+                <h3 className="header-h3">Chicago</h3>
+                <a className="header-text">
+                    We are a family owned Mediterranean restaurant,
+                    focused on traditional recipes served with a mordern twist.
+                </a>
+                <button className="header-button">
+                    Reserve a table
+                </button>
+            </div>
+            <img src={headerLogo} className="header-pic"/>
         </header>
     )
 }
