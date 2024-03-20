9ab93354d12a69a8dcc78432f4513c1be09c0d05
JohannHerberger <j.herberger50@gmail.com>
2024-03-20 03:24:14 +0100

    responisve navbar

--- src/Components/nav.js
+++ src/Components/nav.js
@@ -1,33 +1,61 @@
 import "../styles/nav.css"
 import logo from "../images/Logo.svg"
 import hamburger from "../images/icons8-hamburger-50.png"
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
+import { useState } from "react"
+import cross from "../images/close.png"
 
 export function Nav (){
+  const [mobileNav, setMobileNav] = useState(false)
 
     return(
-        <nav className="navbar">
-            <img className="logo-nav" src={logo} alt="logo"></img>
-            <button className="toggle-button">
+        <nav>
+          <div className="navbar">
+            <img className={mobileNav ? "mobilePassiv": "logo-nav"} src={logo} alt="logo"></img>
+            <button onClick={() => setMobileNav(true)} className={mobileNav ? "mobilePassiv": "toggle-button"}>
                 <img className="hamburger" src={hamburger} />
             </button>
             <ul className="navigation-elements">
                 <li>
-                    <Link to="/" className="nav-link">Home</Link>
+                    <NavLink to="/" className="nav-link">Home</NavLink>
                 </li>
                 <li>
-                    <Link to="/about" className="nav-link">About</Link>
+                    <NavLink to="/about" className="nav-link">About</NavLink>
                 </li>
                 <li>
-                    <Link to="/menu" className="nav-link">Menu</Link>
+                    <NavLink to="/menu" className="nav-link">Menu</NavLink>
                 </li>
                 <li>
-                    <Link to="/reservation" className="nav-link">Reservation</Link>
+                    <NavLink to="/reservation" className="nav-link">Reservation</NavLink>
                 </li>
                 <li>
-                    <Link to="/contact" className="nav-link">Contact</Link>
+                    <NavLink to="/contact" className="nav-link">Contact</NavLink>
                 </li>
             </ul>
+          </div>
+          <div className={mobileNav ? "mobile": "mobilePassiv"}>
+            <button onClick={() => setMobileNav(false)} className={mobileNav ? "toggle-button": "mobilePassiv"}>
+                <img className="close" src={cross} />
+            </button>
+            <ul className="mobile-navigation-elements">
+                <li>
+                    <NavLink to="/" className="nav-link">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about" className="nav-link">About</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/menu" className="nav-link">Menu</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/reservation" className="nav-link">Reservation</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/contact" className="nav-link">Contact</NavLink>
+                </li>
+            </ul>
+            <img className={mobileNav ? "logo-nav-mobile": "mobilePassiv"} src={logo} alt="logo"></img>
+          </div>
         </nav>
     )
 }
