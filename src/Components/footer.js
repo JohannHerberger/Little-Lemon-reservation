2209cb3fac80ea1524be32452f96f1468b68e2a0
JohannHerberger <j.herberger50@gmail.com>
2024-03-27 05:06:25 +0100

    footer, logo and title of page uptaded

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,7 +1,12 @@
 import "../styles/footer.css";
 import logo from "../images/logo2.png"
+import { Link } from "react-router-dom";
 
 export function Footer (){
+    const handleClick = ()=>{
+        window.scrollTo(0,0)
+    };
+
     return(
         <footer className="footer">
             <hr className="line"></hr>
@@ -13,12 +18,31 @@ export function Footer (){
                             <li className="subtitle-footer">
                                 Navigation
                             </li>
-                            <li>Home</li>
-                            <li>About</li>
-                            <li>Menu</li>
-                            <li>Reservation</li>
-                            <li>Order Online</li>
-                            <li>Login</li>
+                            <li>
+                                <Link onClick={() => handleClick()} to="/" style={{color:"black", textDecoration:"none"}}>
+                                    Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link onClick={() => handleClick()} to="/about" style={{color:"black", textDecoration:"none"}}>
+                                    About
+                                </Link>
+                            </li>
+                            <li>
+                                <Link onClick={() => handleClick()} to="/menu" style={{color:"black", textDecoration:"none"}}>
+                                    Menu
+                                </Link>
+                            </li>
+                            <li>
+                                <Link onClick={() => handleClick()} to="/reservation" style={{color:"black", textDecoration:"none"}}>
+                                    Reservation
+                                </Link>
+                            </li>
+                            <li>
+                                <Link onClick={() => handleClick()} to="/contact" style={{color:"black", textDecoration:"none"}}>
+                                    Contact
+                                </Link>
+                            </li>
                         </ul>
                     </div>
                     <div className="footer-wrapper">
@@ -26,9 +50,15 @@ export function Footer (){
                             <li className="subtitle-footer">
                                 Contact
                             </li>
-                            <li>Adress</li>
-                            <li>Phone number</li>
-                            <li>Email</li>
+                            <li>
+                                +1 177 6921 941
+                            </li>
+                            <li>
+                                littleLemon@gmail.com
+                            </li>
+                            <li>
+                                10 S Wacker Dr, Chicago, IL 60606, USA
+                            </li>
                         </ul>
                     </div>
                     <div className="footer-wrapper">
@@ -36,9 +66,15 @@ export function Footer (){
                             <li className="subtitle-footer">
                                 Social Media links
                             </li>
-                            <li>Facebook</li>
-                            <li>Instagram</li>
-                            <li>Twitter</li>
+                            <li>
+                                <a href="https://www.facebook.com/?locale=de_DE" style={{color:"black", textDecoration:"none"}}>Facebook</a>
+                            </li>
+                            <li>
+                                <a href="https://www.instagram.com/" style={{color:"black", textDecoration:"none"}}>Instagram</a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/?lang=de" style={{color:"black", textDecoration:"none"}}>Twitter</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
